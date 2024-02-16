3c951a72e1f827f2a1e054e59cf66c84256a4402
XShe <a1960995065@163.com>
2024-02-16 20:53:30 +0800

    鍒朵綔涓€閿彁浜ゅ伐鍏?

---
+++ git提交(请在文件内修改提交信息).bat
@@ -0,0 +1,5 @@
+git add .
+git commit -m "制作一键提交工具"
+git push origin main
+echo 请观察上述命令有无报错
+pause
---
+++ git提交前置(请确保已加入合作者).bat
@@ -0,0 +1,6 @@
+git remote add origin https://github.com/XSheeee/iceFreezer.git
+git pull origin main
+git branch main
+git checkout main
+echo 请观察上述有无报错
+pause
